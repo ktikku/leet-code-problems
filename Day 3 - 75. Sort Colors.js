cb2f8e66a59d927435698e7f0e6005c8a6c1664c
Karan Tikku <50620678+c285803@users.noreply.github.com>
2020-09-29 19:00:16 +0530

    Day 3 Problem 1

---
+++ Day 3 - 75. Sort Colors.js
@@ -0,0 +1,12 @@
+// Hack
+
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    nums = nums.sort();
+};
+
+
